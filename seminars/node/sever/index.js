0d812deba8ceb6733f9b146cc6adc8cf176c5a17
Polzovatelyande <46621940+Polzovatelyande@users.noreply.github.com>
2019-02-02 14:02:43 +0300

    1

---
+++ seminars/node/sever/index.js
@@ -0,0 +1,11 @@
+let express = require("express");
+let cors  = require("cors");
+ let app = express();
+
+    app.get("/", function(request,response) {
+        let name = request.query.name;
+        let mood = request.query.mood;
+        response.send("Hello, "+ name + "! I am " + mood + " too.");
+    });
+
+    app.listen(591);
